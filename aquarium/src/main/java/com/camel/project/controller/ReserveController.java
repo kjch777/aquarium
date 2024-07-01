76455aff2bb7646ab6268e373b1efa40ccda2d76
오재진 <130115836+renewjin@users.noreply.github.com>
2024-07-01 14:35:33 +0900

    delete login files

--- aquarium/src/main/java/com/camel/project/controller/ReserveController.java
+++ aquarium/src/main/java/com/camel/project/controller/ReserveController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.camel.project.dto.Member;
+//import com.camel.project.dto.Member;
 import com.camel.project.dto.Reserve;
 import com.camel.project.service.ReserveService;
 
